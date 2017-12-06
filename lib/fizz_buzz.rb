da64d17e46884c06e6f7dcdf5a641f5d640e2e1e
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-12-07 00:20:56 +0900

    create fizz buzz

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,27 @@
+def fizz_buzz(n)
+  if (n % 15).zero?
+    'Fizz Buzz'
+  elsif (n % 3).zero?
+    'Fizz'
+  elsif (n % 5).zero?
+    'Buzz'
+  else
+    n
+  end
+end
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(7)
+puts fizz_buzz(8)
+puts fizz_buzz(9)
+puts fizz_buzz(10)
+puts fizz_buzz(11)
+puts fizz_buzz(12)
+puts fizz_buzz(13)
+puts fizz_buzz(14)
+puts fizz_buzz(15)
